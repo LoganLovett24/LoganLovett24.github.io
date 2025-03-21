78f330e7f186c6b29c82325b56f907490f2dfd18
Logan Lovett <LoganLovett24@gmail.com>
2025-03-21 13:17:15 -0500

    CSWD2 B3 Lab 2 Fix 2

--- CSWD2/B3/LAB2/script.js
+++ CSWD2/B3/LAB2/script.js
@@ -1,10 +1,15 @@
 const arrow = document.querySelector('.arrow');
 const speed = document.querySelector('.speed-value');
-
 navigator.geolocation.watchPosition((data) => {
     console.log(data);
-    speed.textContent = data.coords.speed;
-    arrow.style.transform = `rotate(${data.coords.heading}deg)`;
+    let degrees = data.coords.heading
+    speed.textContent = data.coords.speed != null ? data.coords.speed : "SPEED NOT AVAILABLE";
+    if (degrees != null){
+        arrow.style.transform = `rotate(${degrees}deg)`;
+    }
+    else {
+        speed.textContent = speed.textContent+" | ROTATION NOT AVAILABLE"
+    }
 },
 (err) => {
     console.error(err);
